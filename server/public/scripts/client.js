3d19a2980ebef4e50a0868f67fa6e0705672d720
Scott Bromander <scott@primeacademy.io>
2017-03-23 20:54:54 -0500

    object wrapper

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,33 +1,40 @@
-const NUM_BUTTONS = 9;
-const ZERO_BUTTON = true;
+(function CALCULATOR () {
+  const NUM_BUTTONS = 9;
+  const ZERO_BUTTON = true;
 
-$(document).ready(function(){
-  init();
-});
+  $(document).ready(function(){
+    init();
+  });
 
-function init(){
-  appendButtons();
-}
-
-function enable(value){
-  if(value){
-
-  } else {
-
+  function init(){
+    appendButtons();
+    enable(true);
   }
-}
 
-function appendButtons(){
-  if(ZERO_BUTTON){
-    createNumButtons(0);
+  function enable(value){
+    if(value){
+      $("#numButtons").on("click", ".num-button", numClick);
+    } else {
+      $("#numButtons").off("click", ".num-button", numClick);
+    }
   }
-  for(var i = 1; i <= NUM_BUTTONS; i++){
-    createNumButtons(i);
-  }
-}
 
-function createNumButtons(value){
-  $("#numButtons").append("<button class='num-button'>" + value + "</button>");
-  var $el = $("#numButtons").children().last();
-  $el.data("numvalue", value);
-}
+  function numClick(){
+    console.log($(this).data("numvalue"));
+  }
+
+  function appendButtons(){
+    if(ZERO_BUTTON){
+      createNumButtons(0);
+    }
+    for(var i = 1; i <= NUM_BUTTONS; i++){
+      createNumButtons(i);
+    }
+  }
+
+  function createNumButtons(value){
+    $("#numButtons").append("<button class='num-button'>" + value + "</button>");
+    var $el = $("#numButtons").children().last();
+    $el.data("numvalue", value);
+  }
+})();
